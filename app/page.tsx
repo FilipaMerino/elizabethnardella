ed9dc080f8e5e10e3ad5f3678ad8825cbc060994
FilipaMerino <filipaam@outlook.com>
2024-07-27 12:49:35 +0100

    Banner returning props

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Banner } from "./components/banner";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello</p>
+      <Banner src='/homeBanner.svg'/>
     </main>
   );
 }
