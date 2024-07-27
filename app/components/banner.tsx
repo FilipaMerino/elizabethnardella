f3cdc85ad0dd0f1ba86664a61f5addcdc0e904d8
FilipaMerino <filipaam@outlook.com>
2024-07-27 15:39:37 +0100

    Banner Component with props done

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -2,18 +2,13 @@ import Image from 'next/image'
 
 type BannerProps = {
    src: string;
+   bannerQuote: string;
 }
 
 export const Banner = (props: BannerProps) => {
     return(
-        <div>
-            <h1 className='text-5xl'>This is a Banner</h1>
-            <Image
-                src={props.src} 
-                width={500}
-                height={500}
-                alt="A certified counsellor in South Surrey BC offering in-person or online counselling sessions, specialized in art, somatic and narrative therapy, as well as women's counselling and youth(+24). "
-    />
+        <div className='bg-cover bg-center h-[35rem] w-screen flex items-center justify-center' style={{ backgroundImage: `url(${props.src})` }}>
+            <h1 className='text-6xl p-5 text-white'>{props.bannerQuote}</h1>
         </div>
     )
 }
