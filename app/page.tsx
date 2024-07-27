9ece7dbd2e5f77c0c82268c201c139717c1b64b0
FilipaMerino <filipaam@outlook.com>
2024-07-27 15:40:08 +0100

    import banner on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Banner } from "./components/banner";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Banner src='/homeBanner.svg'/>
+      <Banner src='/homeBanner.svg' bannerQuote="Creative Perspectives to Healing"/>
     </main>
   );
 }
