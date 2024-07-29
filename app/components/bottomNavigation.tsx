e55481d3e3a7a15f4bb45fda15f952fbd00ee4f6
FilipaMerino <filipaam@outlook.com>
2024-07-29 16:27:37 +0100

    bottom navigation

--- app/components/bottomNavigation.tsx
+++ app/components/bottomNavigation.tsx
@@ -1,7 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-export const BottomNavigation = () => {
+type BottomNavigationProps = {
+  src1: string;
+  alt1: string;
+  src2: string;
+  alt2: string;
+};
+
+export const BottomNavigation = (props: BottomNavigationProps) => {
   return (
     <div className='px-64 py-16'>
       <div className='flex justify-around'>
@@ -10,28 +17,49 @@ export const BottomNavigation = () => {
             alt=''
             width={200}
             height={200}
-            src='/inPersonConsultations.svg'
+            src={props.src1}
+            alt={props.alt1}
+            className='transition-transform duration-500 ease-in-out transform hover:scale-110'
           />
         </Link>
         <Link href='/consultations'>
           <Image
-            alt=''
+            alt={props.alt2}
             width={200}
             height={200}
-            src='/virtualConsultations.svg'
+            src={props.src2}
+            className='transition-transform duration-500 ease-in-out transform hover:scale-110'
           />
         </Link>
 
         <Link href='/resources'>
-          <Image alt='' width={200} height={200} src='/resources.svg' />
+          <Image
+            alt=''
+            width={200}
+            height={200}
+            src='/resources.svg'
+            className='transition-transform duration-500 ease-in-out transform hover:scale-110'
+          />
         </Link>
 
         <Link href='/blog'>
-          <Image alt='' width={200} height={200} src='/blog.svg' />
+          <Image
+            alt=''
+            width={200}
+            height={200}
+            src='/blog.svg'
+            className='transition-transform duration-500 ease-in-out transform hover:scale-110'
+          />
         </Link>
 
         <Link href='/expertise'>
-          <Image alt='' width={200} height={200} src='/expertise.svg' />
+          <Image
+            alt=''
+            width={200}
+            height={200}
+            src='/expertise.svg'
+            className='transition-transform duration-500 ease-in-out transform hover:scale-110'
+          />
         </Link>
       </div>
     </div>
