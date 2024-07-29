7b025b849be2c557c02bcbd7ae7ae313683da87e
FilipaMerino <filipaam@outlook.com>
2024-07-29 15:14:31 +0100

    Logo with link to homepage

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,37 +1,39 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from 'next/image';
+import Link from 'next/link';
 
 export const Navbar = () => {
-  return(
-    <div className="flex justify-between p-5 items-center">
+  return (
+    <div className='flex justify-between p-5 items-center'>
       <div>
-        <Image
-          src="/logo.png"
-          width={100}
-          height={100}
-          alt="Logo of Elizabeth Nardella Counselling" />
+        <Link href='/'>
+          <Image
+            src='/logo.png'
+            width={100}
+            height={100}
+            alt='Logo of Elizabeth Nardella Counselling'
+          />
+        </Link>
       </div>
-      <div className="flex gap-5">
-        <li className="list-none">
-          <Link href="/about">About</Link>
+      <div className='flex gap-5'>
+        <li className='list-none'>
+          <Link href='/about'>About</Link>
         </li>
-        <li className="list-none">
-          <Link href="/consultations">Consultations</Link>
+        <li className='list-none'>
+          <Link href='/consultations'>Consultations</Link>
         </li>
-        <li className="list-none">
-          <Link href="/expertise">Expertise</Link>
+        <li className='list-none'>
+          <Link href='/expertise'>Expertise</Link>
         </li>
-        <li className="list-none">
-          <Link href="/resources">Resources</Link>
+        <li className='list-none'>
+          <Link href='/resources'>Resources</Link>
         </li>
-        <li className="list-none">
-          <Link href="contact/">Contact Us</Link>
+        <li className='list-none'>
+          <Link href='contact/'>Contact Us</Link>
         </li>
-        <li className="list-none">
-          <Link href="/blog">Blog</Link>
+        <li className='list-none'>
+          <Link href='/blog'>Blog</Link>
         </li>
       </div>
     </div>
-  )
-
-}
+  );
+};
