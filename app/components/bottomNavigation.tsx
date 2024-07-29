ec4b4313cd1e88754a0f60cc0207c51678c3b3d5
FilipaMerino <filipaam@outlook.com>
2024-07-29 15:53:58 +0100

    srcon all the images

--- app/components/bottomNavigation.tsx
+++ app/components/bottomNavigation.tsx
@@ -1,5 +1,25 @@
 import Image from 'next/image';
 
 export const BottomNavigation = () => {
-  return <div></div>;
+  return (
+    <div className='px-64'>
+      <div className='flex justify-around'>
+        <Image
+          alt=''
+          width={200}
+          height={200}
+          src='/inPersonConsultations.svg'
+        />
+        <Image
+          alt=''
+          width={200}
+          height={200}
+          src='/virtualConsultations.svg'
+        />
+        <Image alt='' width={200} height={200} src='/resources.svg' />
+        <Image alt='' width={200} height={200} src='/blog.svg' />
+        <Image alt='' width={200} height={200} src='/resources.svg' />
+      </div>
+    </div>
+  );
 };
