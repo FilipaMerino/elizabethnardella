5ce9b191d8a590256192733681b74d56e8f2da47
FilipaMerino <filipaam@outlook.com>
2024-07-29 14:05:14 +0100

    Primary Button Component with Props

---
+++ app/components/primaryButton.tsx
@@ -0,0 +1,16 @@
+type primaryButtonProps = {
+  name: string;
+  href: string;
+};
+
+export const PrimaryButton = (props: primaryButtonProps) => {
+  return (
+    <div className='py-5'>
+      <a
+        className='btn btn-s sm:btn-md md:btn-lg lg:btn-wide bg-[#219C90] text-white font-light'
+        href={props.href}>
+        {props.name}
+      </a>
+    </div>
+  );
+};
