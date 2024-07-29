5afcc62b6edc9720092d411886ded6b403317fb9
FilipaMerino <filipaam@outlook.com>
2024-07-29 15:12:17 +0100

    Changes in styling of resusable primary button

--- app/components/primaryButton.tsx
+++ app/components/primaryButton.tsx
@@ -5,7 +5,7 @@ type primaryButtonProps = {
 
 export const PrimaryButton = (props: primaryButtonProps) => {
   return (
-    <div className='py-5'>
+    <div className='pt-5'>
       <a
         className='btn btn-s sm:btn-md md:btn-lg lg:btn-wide bg-[#219C90] text-white font-light'
         href={props.href}>
