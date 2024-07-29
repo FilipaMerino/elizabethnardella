86f71a01ba1bffb5aca80faf83e2cf1405c64ccb
FilipaMerino <filipaam@outlook.com>
2024-07-29 16:03:07 +0100

    All images with links on bottom navigation

--- app/components/bottomNavigation.tsx
+++ app/components/bottomNavigation.tsx
@@ -1,24 +1,38 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const BottomNavigation = () => {
   return (
     <div className='px-64'>
       <div className='flex justify-around'>
-        <Image
-          alt=''
-          width={200}
-          height={200}
-          src='/inPersonConsultations.svg'
-        />
-        <Image
-          alt=''
-          width={200}
-          height={200}
-          src='/virtualConsultations.svg'
-        />
-        <Image alt='' width={200} height={200} src='/resources.svg' />
-        <Image alt='' width={200} height={200} src='/blog.svg' />
-        <Image alt='' width={200} height={200} src='/resources.svg' />
+        <Link href='/consultations'>
+          <Image
+            alt=''
+            width={200}
+            height={200}
+            src='/inPersonConsultations.svg'
+          />
+        </Link>
+        <Link href='/consultations'>
+          <Image
+            alt=''
+            width={200}
+            height={200}
+            src='/virtualConsultations.svg'
+          />
+        </Link>
+
+        <Link href='/resources'>
+          <Image alt='' width={200} height={200} src='/resources.svg' />
+        </Link>
+
+        <Link href='/blog'>
+          <Image alt='' width={200} height={200} src='/blog.svg' />
+        </Link>
+
+        <Link href='/expertise'>
+          <Image alt='' width={200} height={200} src='/expertise.svg' />
+        </Link>
       </div>
     </div>
   );
