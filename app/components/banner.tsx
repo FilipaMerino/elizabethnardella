25f2bde8ac721c1b841f8e59343d93d6f8448e10
FilipaMerino <filipaam@outlook.com>
2024-08-09 16:23:52 +0100

    Banner Style Adjustments

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -1,14 +1,16 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 type BannerProps = {
-   src: string;
-   bannerQuote: string;
-}
+  src: string;
+  bannerQuote: string;
+};
 
 export const Banner = (props: BannerProps) => {
-    return(
-        <div className='bg-cover bg-center h-[35rem] w-screen flex items-center justify-center' style={{ backgroundImage: `url(${props.src})` }}>
-            <h1 className='text-6xl p-5 text-white'>{props.bannerQuote}</h1>
-        </div>
-    )
-}
+  return (
+    <div
+      className='bg-cover bg-center h-[35rem] w-screen flex items-center justify-center'
+      style={{ backgroundImage: `url(${props.src})` }}>
+      <h1 className='text-6xl p-5 text-white'>{props.bannerQuote}</h1>
+    </div>
+  );
+};
