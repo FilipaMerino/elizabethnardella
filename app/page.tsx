4c8c044a2916c2d546c6c787b640ee4c41851108
FilipaMerino <filipaam@outlook.com>
2024-08-09 17:03:00 +0100

    Underlining h2 and positining it as a relative position to the h2. Alt Texts for the images

--- app/page.tsx
+++ app/page.tsx
@@ -13,42 +13,54 @@ export default function Home() {
       </div>
 
       <section id='smallIntroduction'>
-        <div className='flex flex-col flex-1'>
-          {/* Here will have an h2 + paragraph bold + paragraph*/}
-          <h2>Life is complicated. Let’s figure it out.</h2>
-          <p>
-            <strong>
+        <div className='flex gap-24 text-justify'>
+          <div className='flex flex-col flex-1'>
+            {/* Here will have an h2 + paragraph bold + paragraph*/}
+            <div>
+              <h2>Life is complicated.</h2>
+              <h2 className='relative'>Let’s figure it out.</h2>
+              <Image
+                src='/underline.png'
+                width={300}
+                height={300}
+                alt='This is an underlining symbol on the home page of Elizabeth Nardella Counselling Website'
+                className='absolute lg:top-[45rem] lg:left-[25rem]'
+              />
+            </div>
+            <p className='italic font-bold mb-3 mt-10'>
               What if caring for your mind could transform your body too?
-            </strong>
-          </p>
-          <p>
-            Life is complicated. Let’s figure it out. What if caring for your
-            mind could transform your body too? If you have been thinking about
-            starting or continuing your counselling journey, but you struggle
-            with the traditional methods of counselling, this is the right place
-            for you. Therapy comes in many forms, and not every approach fits
-            everyone. I specialize in Somatic Therapy, which focuses on the
-            connection between mind and body. We will use mindful movement and
-            body awareness to help you work through your emotions comfortably
-            and effectively.
-          </p>
-          <p>
-            If you feel like this approach resonates with you, book your
-            session. Let's start this beautiful journey together
-          </p>
-          <PrimaryButton
-            name='Book Your Session Now'
-            href='https://elizabethnardellacounselling.janeapp.com/locations/elizabeth-nardella-counselling/book#staff_member/1'
-          />
-        </div>
-        <div className=''>
-          {/* here is gonna be a picture of elizabeth */}
-          <Image
-            alt=''
-            src='/elizabeth-counsellor.png'
-            width={300}
-            height={600}
-          />
+            </p>
+            <p className='mb-5'>
+              If you have been thinking about starting or continuing your
+              counselling journey, but you struggle with the traditional methods
+              of counselling, this is the right place for you.
+            </p>
+            <p className='mb-6'>
+              Therapy comes in many forms, and not every approach fits everyone.
+              I specialize in <strong>Somatic Therapy</strong>, which focuses on
+              the <strong>connection</strong> between <strong>mind</strong> and{' '}
+              <strong>body</strong>. We will use mindful movement and body
+              awareness to help you work through your emotions comfortably and
+              effectively
+            </p>
+            <p className='text-sm italic'>
+              If you feel like this approach resonates with you, book your
+              session. Let's start this beautiful journey together
+            </p>
+            <PrimaryButton
+              name='Book Your Session Now'
+              href='https://elizabethnardellacounselling.janeapp.com/locations/elizabeth-nardella-counselling/book#staff_member/1'
+            />
+          </div>
+          <div className=''>
+            {/* here is gonna be a picture of elizabeth */}
+            <Image
+              alt='Elizabeth smiling ona  field of flowers - Elizabeth Nardella Counselling'
+              src='/elizabeth-counsellor.png'
+              width={300}
+              height={600}
+            />
+          </div>
         </div>
       </section>
     </main>
